f710720d18e9563761e7cf9172ec81ec0b91b432
Alexandr <pesterev.al@yandex.ru>
2021-04-17 17:45:27 +0800

    first commit

---
+++ news/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News
+
+def index(request):
+	news = News.objects.all()
+	context = {
+	'news':news,
+	'title': 'Список новостей'
+	}
+	return render(request, 'news/index.html', context )
